e631f493f905734f61a4ce559e78b2f1f2e5a596
spoonertest <Hua123>
2021-04-17 00:43:45 +0800

    修改 标识

--- ant-design-vue-jeecg/src/components/layouts/TabLayout.vue
+++ ant-design-vue-jeecg/src/components/layouts/TabLayout.vue
@@ -171,7 +171,7 @@
 
       // update-begin-author:sunjianlei date:20200120 for: 动态更改页面标题
       changeTitle(title) {
-        let projectTitle = "Jeecg-Boot 企业级低代码平台"
+        let projectTitle = "智慧农业"
         // 首页特殊处理
         if (this.$route.path === indexKey) {
           document.title = projectTitle
